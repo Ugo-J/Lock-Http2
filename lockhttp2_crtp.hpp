ef573b2b44cd56b288c0c7c55596176064233c1a
Ugo-J <ezeorahJeffrey@gmail.com>
2026-08-20 23:12:03 +0100

    updated the crtp header macro definition

--- lockhttp2_crtp.hpp
+++ lockhttp2_crtp.hpp
@@ -1,5 +1,5 @@
-#ifndef LOCKHTTP2_MAIN_HEADER_HPP
-#define LOCKHTTP2_MAIN_HEADER_HPP
+#ifndef LOCKHTTP2_CRTP_MAIN_HEADER_HPP
+#define LOCKHTTP2_CRTP_MAIN_HEADER_HPP
 
 #ifdef USE_WOLFSSL
     // If the compiled with -DUSE_WOLFSSL, route to the wolfSSL variant
